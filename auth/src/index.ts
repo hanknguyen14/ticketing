b622a47b070c8647227a93bbb4361c5b1dd111a3
Nguyen Manh Hung <hungnguyen.dhg@gmail.com>
2022-10-21 22:15:55 +0700

    feat: implement authenticate strategy

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,26 @@
-import express from "express";
-import {json} from "body-parser";
+import express from 'express';
+import { json } from 'body-parser';
 import 'express-async-errors';
-import { currentUserRouter, signInRouter, signOutRouter, signUpRouter } from "./routes";
-import { errorHandler } from "./middlewares";
-import { NotFoundError } from "./errors";
+import {
+  currentUserRouter,
+  signInRouter,
+  signOutRouter,
+  signUpRouter,
+} from './routes';
+import { errorHandler } from './middlewares';
+import { NotFoundError } from './errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
@@ -16,20 +29,22 @@ app.use(signUpRouter);
 app.use(errorHandler);
 
 app.get('*', () => {
-    throw new NotFoundError();
+  throw new NotFoundError();
 });
 
 const start = async () => {
-    try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
-    } catch (err) {
-        console.error(err);
+  try {
+    if (!process.env.JWT_KEY) {
+      throw new Error('JWT_KEY must be defined');
     }
-    console.log('Connected to MongoDB');
-    app.listen(3000, () => {
-        console.log("listening on port 3000!");
-    });
-}
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  } catch (err) {
+    console.error(err);
+  }
+  console.log('Connected to MongoDB');
+  app.listen(3000, () => {
+    console.log('listening on port 3000!');
+  });
+};
 
 start();
-
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { BadRequestError, RequestValidationError } from '../errors';
 import { User } from '../models';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -31,6 +32,15 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT token
+    const userJWT = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    );
+    req.session = {
+      jwt: userJWT,
+    };
 
     res.status(201).send(user);
   }
