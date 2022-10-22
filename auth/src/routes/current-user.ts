41792fb7d2b1396693bbe6bdbdcbfffefe89a231
Nguyen Manh Hung <hungnguyen.dhg@gmail.com>
2022-10-22 10:51:03 +0700

    feat: create auth current user api

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hello everyone');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: payload });
+  } catch (err) {
+    return res.send({ currentUser: null });
+  }
 });
 
-export { router as currentUserRouter }
+export { router as currentUserRouter };
