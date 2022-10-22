c1680d449c8037af52755d0d9bf63ddffb99f6e3
Nguyen Manh Hung <hungnguyen.dhg@gmail.com>
2022-10-22 11:03:32 +0700

    feat: create auth sign out api

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.post('/api/users/signout', (req, res) => {
-    res.send('Hello everyone');
+router.post('/api/users/signout', (req: Request, res: Response) => {
+  req.session = null;
+  return res.send({});
 });
 
-export { router as signOutRouter }
+export { router as signOutRouter };
