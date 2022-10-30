9d9594a723acd24f549850405c1aefce1e8c446a
Nguyen Manh Hung <hungnguyen.dhg@gmail.com>
2022-10-30 22:54:02 +0700

    feat: init nats client ticket service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from '@dhg-org/common';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -10,6 +11,14 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', 'alsdkj', 'http://nats-srv:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
   } catch (err) {
