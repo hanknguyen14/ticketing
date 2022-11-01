35a61413e91a7d27df17ed8e801ea3354b731a5d
Nguyen Manh Hung <hungnguyen.dhg@gmail.com>
2022-11-01 08:08:40 +0700

    chore: update ticket module env

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -9,9 +9,22 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
 
   try {
-    await natsWrapper.connect('ticketing', 'alsdkj', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
@@ -26,7 +39,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log('listening on port 3000!');
+    console.log('Listening on port 3000!');
   });
 };
 
