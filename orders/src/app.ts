0edb81fc6718a687ac070dc1a07b883251e11dda
Nguyen Manh Hung <hungnguyen.dhg@gmail.com>
2022-11-02 21:22:52 +0700

    feat: implement create order api

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@dhg-org/common';
+import { createOrderRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +15,7 @@ app.use(
   })
 );
 app.use(currentUser);
+app.use(createOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
