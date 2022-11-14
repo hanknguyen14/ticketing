355bf72533afd009c9cd987333ffebdefba1f394
Nguyen Manh Hung <hungnguyen.dhg@gmail.com>
2022-11-14 22:14:48 +0700

    feat: implement client order pages

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,3 +1,4 @@
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 const TicketShow = ({ ticket }) => {
@@ -7,7 +8,8 @@ const TicketShow = ({ ticket }) => {
     body: {
       ticketId: ticket.id,
     },
-    onSuccess: (order) => console.log(order),
+    onSuccess: (order) =>
+      Router.push('/orders/[orderId]', `/orders/${order.id}`),
   });
 
   return (
@@ -15,7 +17,7 @@ const TicketShow = ({ ticket }) => {
       <h1>{ticket.title}</h1>
       <h4>Price: {ticket.price}</h4>
       {errors}
-      <button onClick={doRequest} className="btn btn-primary">
+      <button onClick={() => doRequest()} className="btn btn-primary">
         Purchase
       </button>
     </div>
